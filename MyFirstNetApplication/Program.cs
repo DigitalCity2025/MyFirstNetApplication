943764bcd2d4353935a15e90e737ac466c5e661d
Khun <khun.ly@bstorm.be>
2024-12-23 11:34:50 +0100

    Nombres

--- MyFirstNetApplication/Program.cs
+++ MyFirstNetApplication/Program.cs
@@ -1,7 +1,24 @@
-﻿// Ecrire dans la console
-Console.WriteLine("Hello World !!");
+﻿int age = 42;
+int ageEnBinaire = 0b101010;
+int ageEnHexa = 0x2A;
 
-// Lire dans la console
-string prenom = Console.ReadLine();
+Console.WriteLine(age);
+Console.WriteLine(ageEnBinaire);
+Console.WriteLine(ageEnHexa);
 
-Console.WriteLine("Bonjour " + prenom + " !!");
+long l = 2_000_000_000L * 10;
+
+double value = 3D / 2;
+Console.WriteLine(value);
+
+// decimal
+Console.WriteLine(0.1M * 3);
+Console.WriteLine(1M / 3 * 3);
+
+// float
+Console.WriteLine(0.1F * 3);
+Console.WriteLine(1F / 3 * 3);
+
+// petits nombres
+short val = 16; 
+byte val2 = 8;
