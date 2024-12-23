416a12c7d4d31f6f55a5a7bd2dfb83e8a31d01b2
Khun <khun.ly@bstorm.be>
2024-12-23 14:20:46 +0100

    flags

--- MyFirstNetApplication/Program.cs
+++ MyFirstNetApplication/Program.cs
@@ -1,49 +1,49 @@
-﻿
-// transtypage (cast) implicite
-using System.Globalization;
+﻿bool flag = false;
+bool flag2 = true;
 
-int nb = 42;
-double nb2 = nb;
+int age = 42;
+bool majeur = age > 18;
+
+Console.WriteLine(majeur);
+
+// > plus grand
+// >= plus grand ou égal
+// < plus petit
+// <= plus petit ou égal
+// == egal
+// != différent
+
+Console.WriteLine(42 == age);
+Console.WriteLine(42.Equals(age));
+
+string nom = "   Khun    ".Trim();
+Console.WriteLine(nom.Equals("Khun"));
+Console.WriteLine(nom == "Khun");
+
+Console.WriteLine(nom == "kHuN"); // false
+Console.WriteLine(nom.Equals("kHuN", StringComparison.InvariantCultureIgnoreCase)); // true
+
+// OU inclusif, ET, XOR (OU Exclusif)
+bool jeuneAdulte = age > 18 && age < 30;
+Console.WriteLine(jeuneAdulte);
+
+Console.WriteLine(age == 42 || nom == "Khun"); // OR
+Console.WriteLine(age == 42 ^ nom == "Khun"); // XOR
+
+Console.WriteLine(age == 42 || nom == "Khun");
+
+// changer ou en et
+Console.WriteLine(!(!(age == 42) && !(nom == "Khun")));
+
+// inverser un booléen
+bool test = true; // true
+bool test2 = !test; // false
+
+// loi de morgan
+// !(cond1 && cond2) <=> !cond1 || !cond2
+// !(cond1 || cond2) <=> !cond1 && !cond2
 
 
-// transtypage (cast) explicite
-double nb3 = 42.5;
-int nb4 = (int)nb3;
 
 
-string chaine = "42";
-// un string ne peut être «Caster» en int
-// int nombreEntier = (int)chaine;
 
-int nombreEntier = int.Parse(chaine);
-Console.WriteLine(nombreEntier * 25);
-
-double nombreFlottant = double.Parse("42.05");
-bool flag = bool.Parse("False");
-DateTime date = DateTime.Parse("1982-05-06T12:46:42");
-
-
-string value = 42.ToString();
-string value2 = DateTime.Now.ToString(
-    "dd MMMM yyyy", CultureInfo.GetCultureInfo("fr-BE")
-);
-Console.WriteLine(value2);
-
-// ne fonctionne pas
-//int myNumber = int.Parse("42.3");
-int.TryParse("42.3", out int myNumber);
-Console.WriteLine(myNumber);
-
-string a = string.Empty; // ""
-Console.WriteLine(a);
-
-//Console.WriteLine("Entrez un nombre");
-//int valeurEntree;
-//while (!int.TryParse(Console.ReadLine(), out valeurEntree))
-//{
-//    Console.ForegroundColor = ConsoleColor.Red;
-//    Console.WriteLine("Valeur incorrecte");
-//    Console.ResetColor();
-//}
-
-// Console.WriteLine(valeurEntree);
