6d61f9eb318fa7836fbb9a6842fd74cfcb3210cb
Khun <khun.ly@bstorm.be>
2024-12-23 13:21:26 +0100

    exo2

--- Exercice1_Dates/Program.cs
+++ Exercice1_Dates/Program.cs
@@ -6,3 +6,10 @@ double age = (today - dateNaissance).TotalDays / 365.2425;
 
 Console.WriteLine((int)age);
 Console.WriteLine(Math.Floor(age));
+
+// Calculer l'heure de la pause
+DateTime pause = new DateTime(2024, 12, 23, 15, 0, 0);
+TimeSpan diff = pause - today;
+string pauseFormat = diff.ToString("c");
+Console.WriteLine(pauseFormat);
+Console.WriteLine($"{diff.Hours:D2}:{diff.Minutes:D2}:{diff.Seconds:D2}");
