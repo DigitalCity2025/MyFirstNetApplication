836f386f55bfc82f9214ca5e6c0778294d65daf5
Khun <khun.ly@bstorm.be>
2024-12-23 12:24:44 +0100

    exercice1

---
+++ Exercice1_Dates/Program.cs
@@ -0,0 +1,8 @@
+﻿// Calculer votre age ?
+DateTime today = DateTime.Now;
+DateTime dateNaissance = new DateTime(1990, 05, 31);
+
+double age = (today - dateNaissance).TotalDays / 365.2425;
+
+Console.WriteLine((int)age);
+Console.WriteLine(Math.Floor(age));
