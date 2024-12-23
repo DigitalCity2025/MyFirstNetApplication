ebd9ef33ca65afa92b129dfbcfa18692520d25e6
Khun <khun.ly@bstorm.be>
2024-12-23 11:36:18 +0100

    string

--- MyFirstNetApplication/Program.cs
+++ MyFirstNetApplication/Program.cs
@@ -1,24 +1,45 @@
-﻿int age = 42;
-int ageEnBinaire = 0b101010;
-int ageEnHexa = 0x2A;
+﻿// simple caractère
+char l = 'K';
+string prenom = "Khun";
+string nom = "Ly";
+double prix = 0.1D * 3; // 0.30000000000004
 
-Console.WriteLine(age);
-Console.WriteLine(ageEnBinaire);
-Console.WriteLine(ageEnHexa);
+string phrase = "Bonjour\nles\t\"AMIS\"";
+string phrase2 = @"Bonjour
+les     ""AMIS""";
 
-long l = 2_000_000_000L * 10;
+string directory = "c:\\Users\\Desktop";
+string directory2 = @"c:\Users\Desktop";
 
-double value = 3D / 2;
-Console.WriteLine(value);
 
-// decimal
-Console.WriteLine(0.1M * 3);
-Console.WriteLine(1M / 3 * 3);
+Console.WriteLine(phrase);
+Console.WriteLine(phrase2);
+Console.WriteLine(directory);
+Console.WriteLine(directory2);
 
-// float
-Console.WriteLine(0.1F * 3);
-Console.WriteLine(1F / 3 * 3);
+// concatenation
+Console.WriteLine("Bonjour " + prenom + " " + nom + " !!!");
+// string format
+Console.WriteLine("Bonjour {0} {1} !!!", prenom, nom);
 
-// petits nombres
-short val = 16; 
-byte val2 = 8;
+// string interpolation
+Console.WriteLine($"Bonjour {prenom} {nom} !!!");
+
+
+Console.WriteLine($"Prix : {prix:F2}");
+
+int mois = 1;
+int jour = 5;
+int annee = 2000;
+
+Console.WriteLine($"{jour:D2}/{mois:D2}/{annee}");
+
+string alphabet = "abcdefghijklmnopqrstuvwxyz";
+char lettre = alphabet[4];
+
+Console.WriteLine(lettre);
+
+Console.WriteLine(alphabet[..5]);
+Console.WriteLine(alphabet[23..]);
+Console.WriteLine(alphabet[^3..]);
+Console.WriteLine(alphabet[5..10]);
