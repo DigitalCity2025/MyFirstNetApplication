ab5deb0c60fc2338d83f64e450178ae1a0ef1475
Khun <khun.ly@bstorm.be>
2025-01-07 13:46:42 +0100

    exo6

--- Exercice6_Structures/Program.cs
+++ Exercice6_Structures/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Point?[,] tableau = new Point?[5, 5];
+
+for (int i = 0; i < tableau.GetLength(0); i++)
+{
+    //Point p = new();
+    //p.x = i + 1;
+    //p.y = i + 1;
+    
+    tableau[i, i] = new()
+    {
+        x = i + 1,
+        y = i + 1,
+    };
+}
+
+for (int i = 0; i < tableau.GetLength(0); i++)
+{
+    for (int j = 0; j < tableau.GetLength(1); j++)
+    {
+        Point? p = tableau[i, j];
+        //if (p == null) 
+        //{
+        //    Console.Write("\t");
+        //}
+        //else
+        //{
+        //    Console.WriteLine($"X:{p?.x}-Y:{p?.y}");
+        //}
+        Console.Write(p == null ? "\t" : $"X:{p?.x}-Y:{p?.y}"); 
+    }
+    Console.Write("\n");
+}
+
+struct Point
+{
+    public int x;
+    public int y;
+}
+
+struct Farenheit
+{
+    public double temperature;
+}
+
+struct Celcius
+{
+    public double temperature;
+}
