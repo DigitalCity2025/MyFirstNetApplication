94002ba8560e8f65bfaa284999560f8ce9a383bc
Khun <khun.ly@bstorm.be>
2025-01-09 10:00:01 +0100

    correction fonctions

--- Exercice6_Structures/Program.cs
+++ Exercice6_Structures/Program.cs
@@ -1,35 +1,52 @@
-﻿Point?[,] tableau = new Point?[5, 5];
+﻿//Point?[,] tableau = new Point?[5, 5];
 
-for (int i = 0; i < tableau.GetLength(0); i++)
-{
-    //Point p = new();
-    //p.x = i + 1;
-    //p.y = i + 1;
-    
-    tableau[i, i] = new()
-    {
-        x = i + 1,
-        y = i + 1,
-    };
-}
+//for (int i = 0; i < tableau.GetLength(0); i++)
+//{
+//    //Point p = new();
+//    //p.x = i + 1;
+//    //p.y = i + 1;
 
-for (int i = 0; i < tableau.GetLength(0); i++)
+//    tableau[i, i] = new()
+//    {
+//        x = i + 1,
+//        y = i + 1,
+//    };
+//}
+
+//for (int i = 0; i < tableau.GetLength(0); i++)
+//{
+//    for (int j = 0; j < tableau.GetLength(1); j++)
+//    {
+//        Point? p = tableau[i, j];
+//        //if (p == null) 
+//        //{
+//        //    Console.Write("\t");
+//        //}
+//        //else
+//        //{
+//        //    Console.WriteLine($"X:{p?.x}-Y:{p?.y}");
+//        //}
+//        Console.Write(p == null ? "\t" : $"X:{p?.x}-Y:{p?.y}"); 
+//    }
+//    Console.Write("\n");
+//}
+
+Celcius c = new Celcius { temperature = 30 };
+//Farenheit f = c.ToFarenheit();
+//Console.WriteLine(f.temperature);
+//Celcius c2 = f.ToCelcius();
+//Console.WriteLine(c2.temperature);
+
+Equation equation = new Equation()
 {
-    for (int j = 0; j < tableau.GetLength(1); j++)
-    {
-        Point? p = tableau[i, j];
-        //if (p == null) 
-        //{
-        //    Console.Write("\t");
-        //}
-        //else
-        //{
-        //    Console.WriteLine($"X:{p?.x}-Y:{p?.y}");
-        //}
-        Console.Write(p == null ? "\t" : $"X:{p?.x}-Y:{p?.y}"); 
-    }
-    Console.Write("\n");
-}
+    a = 1,
+    b = -2,
+    c = 1
+};
+
+Console.WriteLine(equation.Resoudre());
+
+
 
 struct Point
 {
@@ -40,9 +57,48 @@ struct Point
 struct Farenheit
 {
     public double temperature;
+
+    public Celcius ToCelcius()
+    {
+        double t = (temperature - 32) / 1.8;
+        return new Celcius { temperature = t };
+    }
 }
 
 struct Celcius
 {
     public double temperature;
+
+    public Farenheit ToFarenheit() 
+    {
+        double t = (temperature * 1.8) + 32;
+        return new Farenheit { temperature = t };
+    }
+}
+
+
+// ax² + bx + c = 0 
+
+
+
+struct Equation
+{
+    public double a, b, c;
+
+    public (bool, double?, double?) Resoudre() 
+    {
+        double delta = (b * b) - (4 * a * c);
+        if(delta < 0)
+        {
+            return (false, null, null);
+        }
+        else
+        {
+            return (
+                true, 
+                (-b + Math.Sqrt(delta)) / (2 * a) ,
+                (-b - Math.Sqrt(delta)) / (2 * a)
+            );
+        }
+    }
 }
--- MyFirstNetApplication/Program.cs
+++ MyFirstNetApplication/Program.cs
@@ -9,3 +9,11 @@ double nb3 = 42.5;
 // transtypage (cast) explicite
 int nb4 = (int)nb3;
 Console.WriteLine("test");
+
+
+// ax² + bx + c
+
+// delta = b² - 4ac
+
+// x1 = (- b + delta^(1/2)) / 2a
+// x2 = (- b - delta^(1/2)) / 2a
