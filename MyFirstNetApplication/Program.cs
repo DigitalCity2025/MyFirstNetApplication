d3c0f0a8f03f56a6ea87d7c6155fa21128b5e447
Khun <khun.ly@bstorm.be>
2025-01-09 10:55:57 +0100

    enum

--- MyFirstNetApplication/Program.cs
+++ MyFirstNetApplication/Program.cs
@@ -1,122 +1,55 @@
-﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+﻿// Diesel, Essence, Gaz , Electricite
 
-Person p;
+// dans une chaine de caractère il n'y a pas de restriction
+string carburant = "glmsjflmg";
 
-// appels de la fonction Parler
-p.Parler();
-p.Parler();
-p.Parler();
-p.Parler();
-p.Parler();
+TypeDeCarburant carbu = TypeDeCarburant.Diesel;
 
-//appels de la fonction Test
-Test();
-Test();
-Test();
-Test();
-Test();
-Test();
+Console.WriteLine((TypeDeCarburant)42);
 
-int value = FactorielDe(5);
-Console.WriteLine(value);
+Console.WriteLine((int)carbu);
 
-void Test()
+Role r = Role.Reassortisseur | Role.Rh | Role.Vendeur;
+Console.WriteLine(r);
+
+// Vérifie si un enum contient un autre enum 
+if(r.HasFlag(Role.Directeur) && r.HasFlag(Role.Vendeur))
 {
-    // définition de la fonction
-    Console.ForegroundColor = ConsoleColor.Yellow;
-    Console.WriteLine("Ceci est un test !!");
-    Console.ResetColor();
+    Console.WriteLine("Bienvenue monsieur le directeur");
 }
 
-int FactorielDe(int nb)
+// transformer un enum en tableau
+TypeDeCarburant[] types = Enum.GetValues<TypeDeCarburant>();
+
+// transformer un string en enum
+TypeDeCarburant type1 = Enum.Parse<TypeDeCarburant>("Diesel");
+
+// transformer un int en enum
+TypeDeCarburant type2 = (TypeDeCarburant)3;
+
+// transformer un enum en int
+int value = (int)TypeDeCarburant.Electricite;
+
+enum TypeDeCarburant
 {
-    int result = 1;
-    for (int i = 2; i <= nb; i++)
-    {
-        result *= i;
-    }
-    return result;
+    Diesel = 42,
+    Essence,
+    Gaz,
+    Electricite
 }
 
-Console.WriteLine(Diviser(1, 42));
-
-double Diviser(double nb1 = 1, double nb2 = 1)
+[Flags]
+enum Role 
 {
-    return nb1 / nb2;
+    Vendeur = 1,
+    Reassortisseur = 2,
+    Rh = 4,
+    Directeur = 8
 }
 
-Test44(nb: 4, s: "");
+// 8 => 1000
+// 4 => 0100
+// 2 => 0010
+// 1 => 0001
 
-void Test44(in string s = "", int nb = 55)
-{
-    // on ne peut plus modifier un paramètre précédé du mot clé in
-    //s = "dswdfsq";
-}
-
-
-double Somme(params double[] values)
-{
-    double result = 0;
-    foreach (var item in values)
-    {
-        result += item;
-    }
-    return result;
-}
-
-int v = 42;
-
-// passer la reference plutot que la valeur
-void UpdateValue(ref int v2)
-{
-    v2 = 45;
-}
-
-UpdateValue(ref v);
-Console.WriteLine(v); //45
-
-
-List<int> list = [1,2,3];
-UpdateList(list);
-
-Console.WriteLine(4.13);
-
-void UpdateList(in List<int> l)
-{
-    l.Add(4);
-}
-
-Console.WriteLine(list.Last());
-
-
-Console.WriteLine("-------------------");
-Console.WriteLine(Somme(42,7,8,9,11,33,99));
-
-
-// retourner plusieurs valeurs
-(int, bool, string , char) Test55555()
-{
-    return (42, true, "Khun🍔🦄", '\n');
-}
-
-(int nb, bool flag, string s, char c) = Test55555();
-(int, bool, string, char) tuple = Test55555();
-Console.WriteLine(tuple.Item1); 
-Console.WriteLine(s);
-
-Console.ReadKey();
-
-
-struct Person
-{
-    // définition de la fonction
-    /// <summary>
-    /// Ecrit dans la console qq chose
-    /// </summary>
-    public void Parler() 
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("Bonjour je m'appelle Khun");
-        Console.ResetColor();
-    }
-}
+// 0001
